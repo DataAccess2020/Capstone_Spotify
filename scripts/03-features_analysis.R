c023fe20df9fef383c0cdf2f051512ebb6cffb14
skorotkikh <s.korotkikh21@gmail.com>
2022-12-08 19:56:00 +0100

    Creates the variables with danceability and instrumentalness data

---
+++ scripts/03-features_analysis.R
@@ -0,0 +1,8 @@
+# Creating the variables with danceability and instrumentalness data
+us_dance <- usa$danceability
+it_dance <- italy$danceability
+jpn_dance <- japan$danceability
+
+us_inst <- usa$instrumentalness
+it_inst <- italy$instrumentalness
+jpn_inst <- japan$instrumentalness
